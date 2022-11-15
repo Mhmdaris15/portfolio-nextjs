72878e55561b3cfd55652f3688f7ff6c5069fc4a
Muhammad Aris Septanugroho <muhammadaris1945@gmail.com>
2022-11-15 11:48:13 +0700

    Initial Commit

---
+++ components/Header.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { SocialIcon } from 'react-social-icons'
+import { motion } from 'framer-motion'
+
+type Props = {}
+
+const Header = (props: Props) => {
+  return (
+    <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-10 xl:items-center'>
+        <motion.div className='flex flex-row items-center'>
+            {/* Social Icons */}
+            <SocialIcon url="https://github.com/mhmdaris15" 
+            fgColor='gray'
+            bgColor='transparent'
+            />
+            <SocialIcon url="https://instagram.com/mhmdaris15" 
+            fgColor='gray'
+            bgColor='transparent'
+            />
+            <SocialIcon url="https://twitter.com/mhmdaris15"
+            fgColor='gray'
+            bgColor='transparent'
+            />
+            <SocialIcon url="https://reddit.com/mhmdaris15" 
+            fgColor='gray'
+            bgColor='transparent'
+            />
+            <SocialIcon url="https://linkedin.com/muhammad-aris-septanugroho" 
+            fgColor='gray'
+            bgColor='transparent'
+            />
+        </motion.div>
+
+        <div className='flex flex-row items-center text-gray-300 cursor-pointer'>
+            <SocialIcon className='cursor-pointer' network='email' fgColor='gray' bgColor='transparent'/>
+            <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>get in touch</p>
+        </div>
+    </header>
+  )
+}
+
+export default Header
