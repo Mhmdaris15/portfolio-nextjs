4ff767470623424bfb3d62269974bcdc898899d8
Muhammad Aris Septanugroho <muhammadaris1945@gmail.com>
2023-01-06 19:43:23 +0700

    Tidying Skills UI

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -10,7 +10,7 @@ function WorkExperience({}: Props) {
       initial={{ opacity: 0}}
       transition={{ duration: 1.2 }}
       whileInView={{ opacity: 1}}
-      className='h-screen relative flex flex-col text-left overflow-hidden md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'>
+      className='h-screen relative flex flex-col text-left overflow-hidden md:flex-row max-w-full px-10 justify-evenly mx-auto items-center space-y-32'>
         <h4 className='absolute top-24 text-gray-500 uppercase text-2xl tracking-[20px] font-bold'>Work Experiences</h4>
         <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory'>
             {/* Experience Card */}
